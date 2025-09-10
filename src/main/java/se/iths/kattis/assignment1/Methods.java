8e967720c7ea9225c886b7e2c3d234a796f51c83
Kattis82 <kattiscalmvik@outlook.com>
2025-09-10 10:44:39 +0200

    Klara för koll

--- src/main/java/se/iths/kattis/assignment1/Methods.java
+++ src/main/java/se/iths/kattis/assignment1/Methods.java
@@ -1,18 +1,52 @@
 package se.iths.kattis.assignment1;
 
-public class Metoder {
+public class Methods {
     public static void main(String[] args) {
 
-        int[] intArray = new int[]{4, 35, 8, 23, 78, 5, 55};
+        // intArray initieras och metoden findMaxInt anropas, en int [] som argument
+        int[] intArray = new int[]{4, 35, 8, 23, 55, 5, 78};
+        System.out.println(findMaxInt(intArray));
 
-        double decimalEtt = 3.5, decimalTva = 6.6;
 
+        // två double initieras och metoden findMaxDouble anropas, två doubles som argument
+        double decimalOne = 3.5, decimalTwo = 6.6;
+        System.out.println(findMaxDouble(decimalOne, decimalTwo));
+
+
+        // två strängar initieras och metoden printFullName anropas, två strängar som argument
         String firstName = "Kattis", lastName = "Calmvik";
+        printFullName(firstName, lastName);
 
 
     }
 
-    private static int 
+    // metoden returnerar en int och har en int [] som parameter
+    private static int findMaxInt(int[] intArray) {
 
+        int max = 0;
+
+        for (int i = 0; i < intArray.length; i++) {
+            if (max < intArray[i]) {
+                max = intArray[i];
+            }
+        }
+        return max;
+    }
+
+    // metoden returnerar en double och har två doubles som parametrar
+    private static double findMaxDouble(double one, double two) {
+
+        if (one > two) {
+            return one;
+        } else {
+            return two;
+        }
+    }
+
+
+    // metoden returnerar ingenting,men har två strängar som parametrar
+    private static void printFullName(String first, String last) {
+        System.out.println(first + " " + last);
+    }
 
 }
--- src/main/java/se/iths/kattis/assignment1/StringArray.java
+++ src/main/java/se/iths/kattis/assignment1/StringArray.java
@@ -1,4 +1,28 @@
 package se.iths.kattis.assignment1;
 
 public class StringArray {
+    public static void main(String[] args) {
+
+        // string array initieras och metoden printArray anropas, en string array som argument
+        String[] stringArray = new String[]{"Hello", "World", "!"};
+        printArray(stringArray);
+
+    }
+    
+
+    // metod som inte returnerar något, en string[] som parameter
+    private static void printArray(String[] array) {
+        String total = "";
+
+        for (int i = 0; i < array.length; i++) {
+            total += array[i];
+            if (i < array.length - 1 && i < array.length - 2) {
+                total += " ";
+            }
+        }
+        System.out.println(total);
+    }
+
+
 }
+
