2c4d051de927ea6908a8637bf489cccca11295f3
Kattis82 <kattiscalmvik@outlook.com>
2025-09-10 08:57:27 +0200

    Initial commit

---
+++ src/main/java/se/iths/kattis/assignment1/Methods.java
@@ -0,0 +1,18 @@
+package se.iths.kattis.assignment1;
+
+public class Metoder {
+    public static void main(String[] args) {
+
+        int[] intArray = new int[]{4, 35, 8, 23, 78, 5, 55};
+
+        double decimalEtt = 3.5, decimalTva = 6.6;
+
+        String firstName = "Kattis", lastName = "Calmvik";
+
+
+    }
+
+    private static int 
+
+
+}
---
+++ src/main/java/se/iths/kattis/assignment1/StringArray.java
@@ -0,0 +1,4 @@
+package se.iths.kattis.assignment1;
+
+public class StringArray {
+}
